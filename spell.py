f2b28c309b073a5145ff10e93bccc0c278b842ed
Ana Burnaska <ahristova.milenova@gmail.com>
2015-06-03 14:30:17 +0300

    some modifiations

--- spell.py
+++ spell.py
@@ -1,7 +1,7 @@
 class Spell:
 
-    def __init__(name="", damage=0, mana_cost=0, cast_range=1):
-        if (type(damage), type(mana_cost), type(cast_range)) not in (int, int, int):
+    def __init__(self, name="", damage=0, mana_cost=0, cast_range=0):
+        if (type(damage), type(mana_cost), type(cast_range)) != (int, int, int):
             raise TypeError("Bad values for spell damage, mana_cost, cast_range")
 
         self.name = str(name)
