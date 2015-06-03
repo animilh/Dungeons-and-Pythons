43e1836678b58fc6b3dd98d308fc4dbcbd14edbb
Ana Burnaska <ahristova.milenova@gmail.com>
2015-06-03 14:28:46 +0300

    added option for spell in fight()

--- fight.py
+++ fight.py
@@ -12,23 +12,29 @@ class Fight:
         self.hero = hero
         self.enemy = enemy
 
-#    @staticmethod
+
     def fight(self, start_weapon):
-        if isinstance(start_weapon, Weapon):
-            while self.hero.get_health > 0 and self.enemy.get_health >0:
+        while self.hero.get_health() > 0 and self.enemy.get_health() > 0:
+            if isinstance(start_weapon, Weapon):
                 self.enemy.take_damage(self.hero.attack(by = "weapon"))
                 print("Hero hits enemy with {} for {} dmg. Enemy health\
                     is {}".format(self.hero.weapon.name, self.hero.weapon.\
                         damage, self.enemy.get_health()))
-                if self.enemy.is_alive():
-                    self.hero.take_damage(self.enemy.get_damage())
-                    print("Enemy hits hero with {} dmg. Hero health is {}".\
-                        format(self.enemy.get_damage(), self.hero.get_health()))
-                    if self.hero.is_alive:
-                        continue
-                    else:
-                        print("Hero is dead")
-                        break
+
+            if isinstance(start_weapon, Spell):
+                self.enemy.take_damage(self.hero.attack(by = "spell"))
+                print("Hero casts a {}, hits enemy for {} dmg. Enemy health\
+                    is {}".format(self.hero.spell.name, self.hero.spell.damage, self.enemy.get_health()))
+
+            if self.enemy.is_alive():
+                self.hero.take_damage(self.enemy.get_damage())
+                print("Enemy hits hero with {} dmg. Hero health is {}".\
+                    format(self.enemy.get_damage(), self.hero.get_health()))
+                if self.hero.is_alive():
+                    continue
                 else:
-                    print("Enemy is dead")
+                    print("{} is dead".format(repr(self.hero)))
                     break
+            else:
+                print("Enemy is dead")
+                break
